00b309f83d3f6aef995f379bc317a4183b2567e3
Gabriel <prieto2208@hotmail.com>
2024-10-28 21:19:07 -0300

    Começando a implementar o JavaScript na aplicação

---
+++ script.js
@@ -0,0 +1,13 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-value").value
+
+    const dolarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
